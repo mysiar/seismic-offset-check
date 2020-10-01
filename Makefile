72e8600e04f5a821efe417104821d9104d2e4d97
Piotr Synowiec <psynowiec@gmail.com>
2020-10-01 11:51:43 +0200

    #db: build tools to include git tag

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 SHELL := /bin/bash
 
+GIT_TAG = $(shell git describe --tags)
+
 build:
 	rm -rf ./build ./dist
 	pyinstaller main.py -n OffsetCheck --windowed
 	cp -r icons/ dist/OffsetCheck
 	cd dist; \
-	tar zcvf ../dist-out/OffsetCheck-linux.tgz OffsetCheck/; \
+	tar zcvf ../dist-out/OffsetCheck-"${GIT_TAG}".linux.tgz OffsetCheck/; \
 	cd ..
 .PHONY: build
+
+var:
+	echo
--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
+@echo off
+for /f %%i in ('git describe --tags') do set GIT_TAG=%%i
+
 rmdir dist /s /q
 rmdir build /s /q
 
 pyinstaller main.py -n OffsetCheck --windowed
 xcopy icons dist\OffsetCheck\icons\
 cd dist
-powershell Compress-Archive OffsetCheck ../dist-out/OffsetCheck-win.zip
+powershell Compress-Archive OffsetCheck ../dist-out/OffsetCheck-%GIT_TAG%.win.zip
 cd ..
