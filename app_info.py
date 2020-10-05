f3ea3f54eccef70d9b16cd0ba5d388e7cf5f91dd
Piotr Synowiec <psynowiec@gmail.com>
2020-10-05 17:27:22 +0200

    #master: license to Help menu

--- app_info.py
+++ app_info.py
@@ -2,7 +2,7 @@
     App Information
 """
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 TITLE = 'Offset Check'
 AUTHOR = 'Piotr Synowiec'
 AUTHOR_EMAIL = 'psynowiec@gmail.com'
@@ -10,3 +10,4 @@ AUTHOR_WEB = 'https://github.com/mysiar'
 
 APP_DATE = 'September 2020'
 HELP_URL = 'https://github.com/mysiar/seismic-offset-check/blob/master/doc/index.md'
+LICENSE_URL = 'https://github.com/mysiar/seismic-offset-check/blob/master/LICENSE'
